7b5f8c8bc34174a34eb45468e0bdb1ef7e2b568a
ЖнецЪ <31236341+jnetc@users.noreply.github.com>
2020-09-11 19:14:53 +0300

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+require("dotenv").config()
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `Jnetc®`,
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-playground`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID, // or process.env.CONTENTFUL_SPACE_ID
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN, // or process.env.CONTENTFUL_TOKEN
+        // spaceId: `if71gryx5ku1`,
+        // accessToken: `Jaxq69jjDoJQtgXqlQ52nmLk7o6TyD9kZlt5ygEgCSM`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
